c58128ed7ccb3b3c2e34b1f45514d848c3437694
unknown <andrew.zavatin@yahoo.com>
2020-06-22 17:27:20 +0300

    build1

--- README.md
+++
@@ -1,1 +0,0 @@
-# raiffeisenonline.github.io
---
+++ precache-manifest.18f2b16bbfc0e8c59faba399db0e1e57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2fda71fd10292bf5169eac8ba04dad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "662bf90173ea4b7967ab",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "78a3e5a89eeee920478e",
+    "url": "/static/css/main.95de27f3.chunk.css"
+  },
+  {
+    "revision": "662bf90173ea4b7967ab",
+    "url": "/static/js/2.c8520c04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c8520c04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a3e5a89eeee920478e",
+    "url": "/static/js/main.e3131bdb.chunk.js"
+  },
+  {
+    "revision": "59a745b08349a4297a22",
+    "url": "/static/js/runtime-main.a8af9357.js"
+  },
+  {
+    "revision": "aeb8b81d9b4853d73583560862463c0a",
+    "url": "/static/media/norton-icon.aeb8b81d.png"
+  }
+]);
